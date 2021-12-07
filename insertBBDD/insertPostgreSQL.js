8e37ec6723a15cfca3a2a3454810478912da5e12
Ericka  Ramos León <eramos@bjumper.com>
2021-12-07 13:22:49 +0100

    insert data to bbdd

--- insertBBDD/insertPostgreSQL.js
+++ insertBBDD/insertPostgreSQL.js
@@ -1,41 +1,73 @@
 async function insertBBDDITA(postgresClient, aggrData) {
-     
-    if (aggrData.dataDC) {
-        console.log("dato de DC",aggrData.dataDC)
-        await postgresClient.query(`DELETE FROM bjumper.dc; DELETE FROM bjumper.room; 
-        DELETE FROM bjumper.row; DELETE FROM bjumper.rack`);
-        for (let data of aggrData.dataDC) {            
-            postgresClient.query(`
-            INSERT INTO bjumper.dc(name, clientid) 
-            VALUES ('${data.label}', '${data.uuid}')`)
+    console.log("data", aggrData)
+    
+    await postgresClient.query(` TRUNCATE TABLE bjumper.dc RESTART IDENTITY CASCADE; 
+        TRUNCATE TABLE bjumper.room RESTART IDENTITY CASCADE;
+        TRUNCATE TABLE bjumper.row RESTART IDENTITY CASCADE; 
+        TRUNCATE TABLE bjumper.rack RESTART IDENTITY CASCADE;       
+    `);
+
+    let insertSql = 'INSERT INTO bjumper.dc(name, clientid) VALUES ';
+
+    let primero = true;
+
+    for (let data of aggrData.dataDC) {       
+        if (primero) {
+            primero = false;
+        } else {
+            insertSql += ', ';
         }
-    } else if ( aggrData.dataRooms) {
-        console.log("dato de ROomS", aggrData.dataRooms)
-        
-        for (let data of aggrData.dataRooms) {       
-            console.log ("dato", data.p_uuid)       
-             postgresClient.query(`
-            INSERT INTO bjumper.room(parentid, name, clientid) 
-            VALUES ((SELECT id from bjumper.dc WHERE clientid ='${data.p_uuid}'),'${data.label}', '${data.uuid}')`)          
+        insertSql += `('${data.label}', '${data.uuid}')`;
+    }    
+
+    await postgresClient.query(insertSql +';');
+
+    insertSql = 'INSERT INTO bjumper.room(parentid, name, clientid) VALUES ';
+
+    primero = true;    
+    
+    for (let data of aggrData.dataRooms) {       
+        if (primero) {
+            primero = false;
+        } else {
+            insertSql += ', ';
         }
-    } else if ( aggrData.dataRows) {
-        console.log("dato de Rows", aggrData.dataRows)
-        
-        for (let data of aggrData.dataRows) {                     
-            postgresClient.query(`
-            INSERT INTO bjumper.row(parentid, name, clientid) 
-            VALUES ((SELECT id from bjumper.room WHERE clientid='${data.parent_uuid}'),'${data.label}', '${data.uuid}')`)          
-        }
-    } else if ( aggrData.dataRacks) {
-        console.log("dato de Racks", aggrData.dataRacks)
-        
-        for (let data of aggrData.dataRacks) {                     
-            postgresClient.query(`
-            INSERT INTO bjumper.rack(parentid, name, clientid, total_u, used_u, free_u) 
-            VALUES ((SELECT id from bjumper.row WHERE name='${data.row}'),'${data.label}', '${data.uuid}',
-            '${data.TOTAL_USPACE}', '${data.USED_USPACE}', '${data.AVAILABLE_USPACE}')`)          
-        }
+        insertSql += `((SELECT id from bjumper.dc WHERE clientid ='${data.p_uuid}'),'${data.label}', '${data.uuid}')`;
     }
+
+    await postgresClient.query(insertSql + ';')
+
+    insertSql = 'INSERT INTO bjumper.row(parentid, name, clientid) VALUES ';
+
+    primero = true;    
+    
+    for (let data of aggrData.dataRows) {       
+        if (primero) {
+            primero = false;
+        } else {
+            insertSql += ', ';
+        }
+        insertSql += `((SELECT id from bjumper.room WHERE clientid='${data.parent_uuid}'),'${data.label}', '${data.uuid}')`;
+    }
+
+    await postgresClient.query(insertSql + ';')
+
+    insertSql = 'INSERT INTO bjumper.rack(parentid, name, clientid, total_u, used_u, free_u, empty) VALUES ';
+
+    primero = true;    
+    
+    for (let data of aggrData.dataRacks) {       
+        if (primero) {
+            primero = false;
+        } else {
+            insertSql += ', ';
+        }
+        insertSql += `((SELECT id from bjumper.row WHERE name='${data.row}'),'${data.label}', '${data.uuid}',
+        '${data.TOTAL_USPACE}', '${data.USED_USPACE}', '${data.AVAILABLE_USPACE}','${data.empty}' )`;
+    }
+    
+
+    await postgresClient.query(insertSql + ';')
 }
 
 module.exports = { insertBBDDITA }
