3d5911a891c77450a3718789c04663dcd763e59e
Ericka  Ramos León <eramos@bjumper.com>
2022-02-24 13:10:04 +0100

    add agreggator File of PUE

---
+++ agreggatorFile/readCSVPUE.js
@@ -0,0 +1,28 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const { produce } = require("../publishRabbit.js");
+
+(async () => {
+  //let writeCSV;
+  const dcs = [];
+  try {
+    fs.createReadStream("./file/PUE.csv")
+      .pipe(csv({}))
+      .on("data", (data) => dcs.push(data))
+      .on("end", () => {
+        console.log("dcs", dcs);
+        for (let dc of dcs) {
+          produce(
+            JSON.stringify({
+              type: "AgreggatorFile",
+              dataDC: dc,
+              dataRooms: "hay un room",
+            })
+          );
+        }
+      });
+  } catch (e) {
+    console.error("Error Occurred", e);
+    throw new Error(e);
+  }
+})();
